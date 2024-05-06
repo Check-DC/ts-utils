777089c646523cd49fa6f559541f650a6118b15d
Kamsi-yonna <etiobikamsi@gmail.com>
2024-05-06 17:30:38 +0100

    feat - setup index page as entry for all functions

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+export { isArr, isArrEmpty, pushOrUpdate, pushUniqueValue } from "../src/array";
+
+export { formatDate, fromNow, toNow } from "../src/date";
+
+export {
+  addSeparators,
+  addUnit,
+  ensureNumber,
+  formatNumber,
+  numberEmptyState,
+  parseFormat,
+  parseNumber,
+  toFixed,
+} from "../src/number";
+
+export {
+  deepClone,
+  hasKeysAndValues,
+  parseJSON,
+  parseNumValues,
+  parseNumericObj,
+  querylize,
+  removeEmptyKeys,
+  removeKeys,
+  renameKey,
+  renameKeys,
+  reserveKeys,
+  sanitizeQuery,
+  sortByKeyLength,
+} from "../src/object";
+
+export {
+  generateUuid,
+  initials,
+  randomStr,
+  toFullUrl,
+  toLowerCase,
+  toUpperCase,
+} from "../src/string";
+
+// import * as stringUtils from "../src/string";
+// import * as arrayUtils from "../src/array";
+// import * as dateUtils from "../src/date";
+// import * as numberUtils from "../src/number";
+// import * as objectUtils from "../src/object";
+
+// index.ts
+// export const tsUtils = {
+//   ...stringUtils,
+//   ...arrayUtils,
+//   ...dateUtils,
+//   ...numberUtils,
+//   ...objectUtils,
+// };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/string.ts"],
+  entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
   splitting: false,
